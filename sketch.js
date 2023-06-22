5e1525aa493caa40789bc70155baa46505293e31
milosh3411 <milos.radenkovic@microcloud.rs>
2023-06-23 00:13:41 +0200

    v1

--- sketch.js
+++ sketch.js
@@ -9,17 +9,26 @@ let goal_sum;
 let current_sum;
 let coin;
 let vis;
+let font;
+let brand;
+let qr;
+function preload() {
+  font = loadFont("data/Alice-Regular.ttf");
+  brand = loadImage("data/150.png");
+  qr = loadImage("data/qr.png");
+}
 function setup() {
   w = 540;
-  h = 960;
+  h = 1200;
   createCanvas(w, h);
   vis = createGraphics(w, h);
   drawing_left_margin = 40;
   drawing_right_margin = 40;
   drawing_top_margin = 100;
-  drawing_bottom_margin = 160;
+  drawing_bottom_margin = 200;
   drawing_width = w - drawing_left_margin - drawing_right_margin;
   drawing_height = h - drawing_top_margin - drawing_bottom_margin;
+  vis.textFont(font);
   vis.noLoop()
   noLoop();
   goal_sum = createInput();
@@ -55,30 +64,20 @@ function getDonatedSum() {
   return 4000;
 }
 function toCurrency(num) {
-  let cur = new Intl.NumberFormat('sr-SR', { style: 'currency', currency: 'RSD' }).format(num);
-  return cur;
-}
-function box_filled(x, y, bw, bh) {
-  let r = random(255);
-  let g = random(255);
-  let b = random(255);
-  vis.fill(r, g, b, 50);
-  vis.stroke(r, g, b, 50);
-  vis.rect(x, y, bw, -bh);
-  let circle_radius = 0.5 * min(bw, bh);
-  vis.fill(255, 255, 255, 50);
-  vis.stroke(255, 255, 255, 50);
-  vis.circle(x + 0.5 * bw, y - 0.5 * bh, circle_radius);
+  let cur = new Intl.NumberFormat('sr-SR').format(num);
+  return cur.toString().replace(/\./g, " ") + " дин. ";
 }
 function box_filled1(x, y, bw, bh) {
-//draw png image from data/coin.png
- // vis.image(coin, x, y, bw, -bh);
 //draw circle
   let circle_radius = 0.9 * min(bw, bh);
   //dark yellow
   vis.fill(242, 160, 7);
   vis.stroke(255, 255, 255, 50);
   vis.circle(x + 0.5 * bw, y - 0.5 * bh, circle_radius);
+  //light yellow
+  vis.fill(242, 242, 7);
+  //small circle inside
+  vis.circle(x + 0.55 * bw, y - 0.55 * bh, 0.2 * circle_radius);
 }
 function box_empty(x, y, bw, bh) {
     let circle_radius = 0.9 * min(bw, bh);
@@ -87,58 +86,121 @@ function box_empty(x, y, bw, bh) {
     vis.stroke(255, 255, 255, 50);
     vis.circle(x + 0.5 * bw, y - 0.5 * bh, circle_radius);
 }
+function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  vis.beginShape();
+  for (let a = -PI/16; a < TWO_PI - PI/16; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vis.vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vis.vertex(sx, sy);
+  }
+  vis.endShape(CLOSE);
+}
 function visualize() {
+  let n = 24;
+  let m = 42;
+  let rect_width = drawing_width / n;
+  let rect_height = drawing_height / m;
   let date = new Date();
   let day = date.getDate();
   let month = date.getMonth() + 1;
   let year = date.getFullYear();
   let current_date = `${day}-${month}-${year}`
+  //brand.resize(0.17 * drawing_width, 0.17 * drawing_width)
+  //brand.resize(.8*drawing_width,0.8*drawing_width)
   vis.clear();
   vis.smooth();
   vis.background(250, 250, 250);
+  //vis.image(brand, drawing_left_margin - 15, 5)
   vis.rectMode(CORNER);
   vis.stroke(0, 0, 0, 150);
   vis.fill(0, 0, 0, 150);
   vis.push();
   vis.translate(drawing_left_margin, h - drawing_bottom_margin);
+  //vis.image(brand, 0.1*drawing_width, -0.5*drawing_height)
   vis.noFill()
   vis.stroke(0, 0, 0, 150);
   vis.rectMode(CORNER);
-  vis.rect(-5, 5, drawing_width+10, -drawing_height-10-0.45*drawing_top_margin,10,10,10,10);
-  vis.stroke(250, 250, 250);
   vis.strokeWeight(3)
-  vis.line(75, -drawing_height-5-0.45*drawing_top_margin, drawing_width - 75, -drawing_height-5-0.45*drawing_top_margin);
-  let n = 34;
-  let m = 60;
+  vis.rect(-10, 10, drawing_width+20, -m * rect_height,10,10,10,10);
+  vis.stroke(250, 250, 250);
+  vis.strokeWeight(5)
+  vis.line(60, -m * rect_height + 10, drawing_width - 60, -m * rect_height + 10);
   let last_row = 0;
-  let box_price = getGoalSum() / (n * m);
-  let rect_width = drawing_width / n;
-  let rect_height = drawing_height / m;
+  let count = 0;
+  let box_price = getGoalSum() / 1000;
+  let lx = 0*rect_width;
+  let ly = 2*rect_height;
+  vis.fill(255,69,0);
+  vis.stroke(255, 255, 255, 50);
+  star(lx + 0.5 * rect_width, ly - 0.5 * rect_height, 0.25 * rect_width, .5*rect_width, 5);
+  vis.textSize(16)
+  vis.text(toCurrency(box_price),lx + 0.5 * rect_width + 10, ly - 0.5 * rect_height + 5);
+
   coin.resize(rect_width, rect_height)
-  for (let i = 0; i < m; i++) {
+  for (let i = 0; i < m - 1; i++) {
     for (let j = 0; j < n; j++){
-      let count = i * n + j;
+      count++;
       let x = j * rect_width;
       let y = -i * rect_height;
-      if (count*box_price < getCurrentSum()) {
-        box_filled1(x, y, rect_width, rect_height);
+      if (count*box_price <= getCurrentSum()) {
+        vis.fill(255,69,0);
+        vis.stroke(255, 255, 255, 50);
+        star(x + 0.5 * rect_width, y - 0.5 * rect_height, 0.25 * rect_width, .5*rect_width, 5);
         last_row = i;
       } else {
-        box_empty(x, y, rect_width, rect_height);
+        vis.fill(232, 232, 232);
+        vis.stroke(255, 255, 255, 50);
+        star(x + 0.5 * rect_width, y - 0.5 * rect_height, 0.25 * rect_width, .5*rect_width, 5);
       }
     }
   }
+  i = m - 1;
+  for (let j = 4; j < n - 4; j++){
+    count++;
+    let x = j * rect_width;
+    let y = -i * rect_height;
+    if (count*box_price <= getCurrentSum()) {
+      vis.fill(255,69,0);
+      vis.stroke(255, 255, 255, 50);
+      star(x + 0.5 * rect_width, y - 0.5 * rect_height, 0.25 * rect_width, .5*rect_width, 5);
+      last_row = i;
+    } else {
+      vis.fill(232, 232, 232);
+      vis.stroke(255, 255, 255, 50);
+      star(x + 0.5 * rect_width, y - 0.5 * rect_height, 0.25 * rect_width, .5*rect_width, 5);
+    }
+  }
+
   vis.pop();
-  vis.fill(0, 0, 0, 150);
-  vis.noStroke()
-  vis.textFont('Courier New');  
+  vis.fill(0, 0, 0, 100);
   vis.textSize(drawing_top_margin / 3.5);
   vis.rectMode(CENTER);
   vis.textAlign(CENTER, CENTER);
+  vis.textSize(42);
+  vis.text("1000    за...", w / 2, 20);
   vis.textSize(32);
-  vis.text(toCurrency(getGoalSum()), w / 2, drawing_top_margin / 2);
-  vis.textSize(drawing_top_margin / 3.5);
-  vis.text(current_date, w / 2, h - drawing_bottom_margin / 4);vis.text(toCurrency(getCurrentSum()), w / 2, h - drawing_bottom_margin - last_row*rect_height - drawing_top_margin / 3.5);
-  
+  vis.text("нови понтон!", w / 2, 50);
+  vis.fill(255,69,0);
+  vis.stroke(255, 255, 255, 50);
+  star(w/2 + 10, 30, 0.4 * rect_width, .8*rect_width, 5);
+  vis.textSize(24);
+  vis.noStroke()  
+  vis.fill(0, 0, 0, 150);
+  vis.text(toCurrency(getCurrentSum()) + current_date, w / 2, h - drawing_bottom_margin - last_row*rect_height - 1.5 * rect_height);
+  vis.textSize(12);
+  vis.text("studio@microcloud.rs", w / 2, h - drawing_bottom_margin / 8);
+  vis.textAlign(LEFT, CENTER);
+  vis.textSize(16);
+  vis.text("145-0000000026223-70", drawing_left_margin, h - drawing_bottom_margin + 60);
+  vis.text("Веслачки клуб Црвена Звезда", drawing_left_margin, h - drawing_bottom_margin + 80);
+  vis.text("Ада Циганлија 5", drawing_left_margin, h - drawing_bottom_margin + 100);
+  vis.text("11030 Београд", drawing_left_margin, h - drawing_bottom_margin + 120);
+  vis.tint(255, 255, 255, 150);
+  vis.image(qr, w - drawing_right_margin - 120, h - drawing_bottom_margin + 30, 130, 130);
   saveCanvas(vis, current_date, 'jpg')
 }
